41e1f1d06c5d80f03fab8406af1d65cfc26722a2
anchit1501 <1501.anchit@gmail.com>
2019-07-28 16:24:48 +0530

    Session and Logout Added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 // require('./routes/authRoutes');
 require('./models/user');
@@ -9,6 +11,16 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI,()=>console.log('connected to db'))
 
 const app = express();
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 require('./routes/authRoutes')(app);
 
